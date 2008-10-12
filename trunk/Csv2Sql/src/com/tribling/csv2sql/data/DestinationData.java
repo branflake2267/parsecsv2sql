c28fb4898743581d77f55e2464ccf3591ffaa52f
branflake2267@gmail.com <branflake2267@gmail.com@42eeab0e-955f-11dd-9df2-0dfe14157e45>
2008-10-12 18:17:45 +0000

    added only optimise to varchar/text

--- trunk/Csv2Sql/src/com/tribling/csv2sql/data/DestinationData.java
+++ trunk/Csv2Sql/src/com/tribling/csv2sql/data/DestinationData.java
@@ -1,21 +1,35 @@
 package com.tribling.csv2sql.data;
 
+/**
+ * Destination values - controls insert/update methods
+ * 
+ * @author branflake2267
+ *
+ */
 public class DestinationData {
 
-	// drop table before insert
+	// drop table before insert 
+	// (if looping files in a directory, it will drop table in the beginning)
 	public boolean dropTable = false;
 	
-	// skipping optimising table (you can do this sparately if needed.)
+	// auto optimise at the end of inserting/updating records
+	// this will skip optimising table and can do this separately if needed.
 	public boolean optimise = false;
 	
-	// how many records to examine
+	// how many records to examine for optimisation of the column data
+	// records are examine randomly over this many records
 	public int optimiseRecordsToExamine = 1000;
+	
+	// optimise lengths into varchar and text only
+	// this will have to be done first every time, so that
+	// one can go from varchar->date, varchar->int (instead of text>date=will not work)
+	public boolean optimiseTextOnly = true;
 	
 	// delete empty columns after parsing a table
 	public boolean deleteEmptyColumns = false;
 	
 	
-	// [MySql|MsSql]
+	// [MySql|MsSql] - MySql or Microsoft SQL databases methods available
 	public String databaseType = "MySql";
 	
 	// datbase settings
@@ -25,11 +39,12 @@ public class DestinationData {
 	public String password;
 	public String port;
 	
+	// table for records to exist
 	public String table;
-	public MatchFieldData[] identityColumns;
 
+	// this only appears in MsSql that I know of. select * from [catalog].[tableSchema].[table]
 	public String tableSchema;
 
-	
-	
+	// identiy columns to match update instead of insert?
+	public MatchFieldData[] identityColumns;
 }
