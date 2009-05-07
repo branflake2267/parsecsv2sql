94be2823b00da3e0dd08e164ef6e72ab651d2f51
branflake2267@gmail.com <branflake2267@gmail.com@42eeab0e-955f-11dd-9df2-0dfe14157e45>
2009-05-07 02:02:54 +0000

    added more tests

--- trunk/Csv2Sql/test/com/tribling/csv2sql/test/lib/DateParserTests.java
+++ trunk/Csv2Sql/test/com/tribling/csv2sql/test/lib/DateParserTests.java
@@ -11,7 +11,28 @@ public class DateParserTests {
 
   public void run() {
     
+    // test dates 
+    
+    parse.test_EngDate("04/01/2008", "04/01/2008");
+    parse.test_EngDate("04-01-08", "04/01/2008");
+    parse.test_EngDate("4-5-09", "04/05/2009");
+    parse.test_EngDate("5/6/2009", "05/06/2009");
     parse.test_EngDate("3-apr-09", "04/03/2009");
+    parse.test_EngDate("may 2009", "05/01/2009");
+    parse.test_EngDate("apr 08", "04/01/2008");
+    parse.test_EngDate("January 01, 2009", "01/01/2009");
+    parse.test_EngDate("20091203000000", "12/03/2009");
+    
+    
+    parse.test_MySqlDate("jan 2009", "2009-01-01 00:00:00");
+    parse.test_MySqlDate("2009-12-01 01:02:01", "2009-12-01 01:02:01");
+    parse.test_MySqlDate("2009-12-01 00:00:00AM", "2009-12-01 00:00:00"); 
+    parse.test_MySqlDate("02/23/2009 11:14:31", "2009-02-23 11:14:31"); 
+    parse.test_MySqlDate("04/03/2009 01:09:34 PM", "2009-04-03 13:09:34"); 
+    
+    
+    
+    parse.test_MySqlDate("02/23/09 11:14 am", "2009-02-23 11:14:00"); 
     
   }
   
