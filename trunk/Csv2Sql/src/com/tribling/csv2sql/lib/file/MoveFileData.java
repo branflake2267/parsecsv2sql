ac891a09f1d2c7698984f6a46b19ffee2bb04ba3
branflake2267@gmail.com <branflake2267@gmail.com@42eeab0e-955f-11dd-9df2-0dfe14157e45>
2009-05-23 18:45:38 +0000

    added automated move file on header matching

---
+++ trunk/Csv2Sql/src/com/tribling/csv2sql/lib/file/MoveFileData.java
@@ -0,0 +1,63 @@
+package com.tribling.csv2sql.lib.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.csvreader.CsvReader;
+
+public class MoveFileData {
+
+  public String matchHeaderValues = null;
+  
+  public char delimiter;
+  
+  public String pathToMoveToDir = null;
+  
+  /**
+   * set a sample file to get the matchheaderValues from
+   * 
+   * @param fileOrdir
+   */
+  public void setMatchHeaderValues(File fileOrdir) {
+   
+    File file = null;
+    if (fileOrdir.isDirectory()) {
+      file = fileOrdir.listFiles()[0];
+    } else {
+      file = fileOrdir;
+    }
+    
+    CsvReader reader = null;
+    try {     
+      reader = new CsvReader(file.toString(), delimiter);
+    } catch (FileNotFoundException e) {
+      System.err.println("doesFileHeaderMatchStr: Could not open CSV Reader");
+      e.printStackTrace();
+    }
+    
+    if (reader == null) {
+      matchHeaderValues = null;
+    }
+    
+    String[] header = null;
+    try {
+      header = reader.getHeaders();
+    } catch (IOException e) {
+      System.out.println("doesFileHeaderMatchStr: could not read headers");
+      e.printStackTrace();
+    }
+    
+    if (header == null) {
+      matchHeaderValues = null;
+    }
+    
+    String sheader = "";
+    for (int i=0; i < header.length; i++) {
+      sheader += header[i];
+    }
+    
+    matchHeaderValues = sheader;
+  }
+  
+}
