71d22e482c4bf4142b7e31891991135a09818cac
branflake2267 <branflake2267@42eeab0e-955f-11dd-9df2-0dfe14157e45>
2009-12-01 03:37:16 +0000

    adding more unit testing

--- Csv2Sql/unittests/org/gonevertical/csv2sql/lib/sql/MsSqlQueryUtilTest.java
+++ Csv2Sql/unittests/org/gonevertical/csv2sql/lib/sql/MsSqlQueryUtilTest.java
@@ -2,6 +2,11 @@ package org.gonevertical.csv2sql.lib.sql;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.gonevertical.csv2sql.data.ColumnData;
 import org.gonevertical.csv2sql.data.DatabaseData;
 import org.junit.After;
@@ -10,31 +15,63 @@ import org.junit.Test;
 
 public class MsSqlQueryUtilTest {
 
-  private DatabaseData databaseData;
+  private DatabaseData dd;
 
   @Before
   public void setUp() throws Exception {
-    databaseData = new DatabaseData(DatabaseData.TYPE_MYSQL, "ark", "3306", "test", "test#", "test");
+    dd = new DatabaseData(DatabaseData.TYPE_MYSQL, "ark", "3306", "test", "test#", "test");
   }
 
   @After
   public void tearDown() throws Exception {
-    databaseData = null;
+    dd = null;
   }
 
   @Test
   public void testEscapeString() {
-    fail("Not yet implemented"); // TODO
+    String value = "mystring\\";
+    String s = MySqlQueryUtil.escape(value);
+    String sql = "INSERT INTO test_escape SET Value='" + s + "'";
+    long id = MySqlQueryUtil.update(dd, sql);
+    sql = "SELECT Value FROM test_escape WHERE TestId='" + id + "';";
+    String valueTest = MySqlQueryUtil.queryString(dd, sql);
+    assertEquals(value, valueTest);
   }
 
   @Test
   public void testEscapeInt() {
-    fail("Not yet implemented"); // TODO
+    String s = MySqlQueryUtil.escape(1);
+    String sql = "INSERT INTO test_escape SET Value='" + s + "'";
+    long id = MySqlQueryUtil.update(dd, sql);
+    sql = "SELECT Value FROM test_escape WHERE TestId='" + id + "';";
+    String valueTest = MySqlQueryUtil.queryString(dd, sql);
+    assertEquals(Integer.toString(1), valueTest);
   }
 
   @Test
   public void testGetResultSetSize() {
-    fail("Not yet implemented"); // TODO
+    String sql = "SELECT COUNT(*) AS t FROM test_escape;";
+    int count = MySqlQueryUtil.queryInteger(dd, sql);
+    sql = "SELECT * FROM test_escape;";
+    int resultSize = 0;
+    try {
+      Connection conn = dd.getConnection();
+      Statement select = conn.createStatement();
+      ResultSet result = select.executeQuery(sql);
+      resultSize = MySqlQueryUtil.queryInteger(dd, sql);
+      result.close();
+      result = null;
+      select.close();
+      select = null;
+      conn.close();
+    } catch (SQLException e) {
+      System.err.println("Error: " + sql);
+      e.printStackTrace();
+    } 
+    if (count == 0 | resultSize == 0) {
+      count = -1;
+    }
+    assertEquals(count, resultSize);
   }
 
   @Test
