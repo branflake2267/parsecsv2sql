20854f38ac6f39472ede494d9ffe9d166b90d9fe
branflake2267 <branflake2267@42eeab0e-955f-11dd-9df2-0dfe14157e45>
2009-12-13 01:03:45 +0000

    package rename

--- Csv2Sql/src/org/gonevertical/csv2sql/data/IdentityData.java
+++
@@ -1,9 +0,0 @@
-package org.gonevertical.csv2sql.data;
-
-public class IdentityData {
- 
-	public String column = null;
-	
-	public String value = null;
-	
-}
--- Csv2Sql/src/org/gonevertical/csv2sql/lib/openoffice/RowData.java
+++
@@ -1,7 +0,0 @@
-package org.gonevertical.csv2sql.lib.openoffice;
-
-public class RowData {
-
-  public String[] cell = null;
-  
-}
