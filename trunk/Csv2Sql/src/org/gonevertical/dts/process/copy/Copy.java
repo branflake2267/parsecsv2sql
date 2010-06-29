305ec564bbf17c00c0cfb92ecf7b112a7b71cb3c
branflake2267 <branflake2267@42eeab0e-955f-11dd-9df2-0dfe14157e45>
2010-06-29 13:15:09 +0000

    testing sql parameter delineation using regex, for translating insert into statements in mysql format to ms sql format

---
+++ trunk/Csv2Sql/src/org/gonevertical/dts/process/copy/Copy.java
@@ -0,0 +1,18 @@
+package org.gonevertical.dts.process.copy;
+
+public class Copy {
+
+	private String tableLeft;
+	private String tableRight;
+
+	public Copy() {
+	}
+	
+	public void setTables(String tableLeft, String tableRight) {
+		this.tableLeft = tableLeft;
+		this.tableRight = tableRight;
+	}
+	
+	
+	
+}
